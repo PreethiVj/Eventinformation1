21d0928d10eac817c7b31ac34352c8e82f2b1f58
Preethi.N <preethi.n@engage2serve.com>
2023-11-10 15:33:00 +0530

    i have added one commit req

---
+++ src/demo/SampleClass.java
@@ -0,0 +1,39 @@
+package demo;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+
+//		
+//		String name1 ="vithurshana";
+//		StringBuffer name2= new StringBuffer();
+//		 name2.append(name1);
+//		 StringBuffer store= name2.reverse();
+//		 System.out.println(store);
+//       }
+		
+
+
+//		int x=10, y=20,z= 0;
+//		z=x;
+//		x=y;
+//		y=z;
+//		System.out.println(x +"test"+ y);
+
+		int x=10, y=20;
+		x=x+y; //30
+		y=x-y;  //10
+		x=x-y;  // 20
+		
+		
+		System.out.println(x + ""+ y);
+		
+		
+	
+	}
+	
+	
+		
+	}
+
+
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author preethi
+ *
+ */
+module Newjava {
+}
